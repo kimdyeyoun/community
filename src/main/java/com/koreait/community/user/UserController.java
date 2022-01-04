63a345baeb8c8d519c6150be68cc7757ecc5f2ad
eodyd <eodyd7072@naver.com>
2022-01-04 16:42:10 +0900

    spring git

---
+++ src/main/java/com/koreait/community/user/UserController.java
@@ -0,0 +1,17 @@
+package com.koreait.community.user;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/join")
+    public void join(){};
+}
